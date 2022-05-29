2bd356a186547f4ca5bdfc33f3d11c89e84fb3b8
DevDokus <49053928+DevDokus@users.noreply.github.com>
2022-05-29 18:36:06 +0200

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "adamant"
+game "rdr3"
+
+this_is_a_map "yes"
+
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+--mh1_01.ymap
